db6d97c7ab581ee5804ffd3cc92b71355f60d0f7
yo <jcgvicto@gmail.com>
2014-10-26 11:33:55 +0100

    initial ecro software commit

---
+++ ecro/software/LaunchEcro.hpp
@@ -0,0 +1,51 @@
+// -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*-
+
+#ifndef __RUN_BOT__
+#define __RUN_BOT__
+
+#include <yarp/os/RFModule.h>
+#include <yarp/os/Module.h>
+#include <yarp/os/Network.h>
+#include <yarp/os/Port.h>
+#include <yarp/os/BufferedPort.h>
+
+#include <yarp/dev/PolyDriver.h>
+
+#include "RdMacros.hpp"
+
+#define DEFAULT_TYPE "pwmbot"
+#define DEFAULT_PREFIX "0/ecro"
+#define DEFAULT_CAMERA "on"
+
+using namespace yarp::os;
+using namespace yarp::dev;
+
+/**
+ * @ingroup LaunchEcro
+ *
+ * The LaunchEcro class tests the Pwm class as a controlboard.
+ * 
+ */
+class LaunchEcro : public RFModule {
+
+    public:
+        LaunchEcro();
+        bool configure(ResourceFinder &rf);
+
+    protected:
+        yarp::dev::PolyDriver cameraDevice;
+        yarp::dev::PolyDriver robotDevice;
+
+        virtual bool updateModule();
+        virtual bool close();
+
+//        double getPeriod();
+//        bool interruptModule();
+//        int period;
+
+        std::string camera;
+
+};
+
+#endif  // __RUN_BOT__
+
