2ab0a71d033081fccf542fc42a3526be12602d82
jgvictores <jcgvicto@gmail.com>
2015-02-02 19:35:11 +0100

    size as CD_SIZE_T

--- rd1/software/pwmbot/PwmBot.cpp
+++ rd1/software/pwmbot/PwmBot.cpp
@@ -6,7 +6,7 @@
 
 bool PwmBot::indexWithinRange(const int& idx) {
     if (idx >= gpios.size() ){
-        CD_WARNING("Index out of range!! (%d >= %d)!!!\n",idx,gpios.size());
+        CD_WARNING("Index out of range!! (%d >= " CD_SIZE_T ")!!!\n",idx,gpios.size());
         return false;
     }
     return true;
