675cc835c72b154ed1113e962b1261bde2f777ce
jgvictores <jcgvicto@gmail.com>
2016-11-23 15:57:12 +0100

    recover firmware from dead-newApproach as suggested in #18

--- laserTowerOfDeath/firmware/firmware.ino
+++ laserTowerOfDeath/firmware/firmware.ino
@@ -1,67 +1,110 @@
-//--------------------------------------------------------------
-//-- A server for byte-sized commands, first 2 most significant
-//-- bits reserved for direction, rest for PWM ms.
-//--------------------------------------------------------------
-//-- (c) Juan G Victores, Jun 2012
-//-- (c) Juan Gonzalez-Gomez (Obijuan), Dec 2011
-//-- GPL license
-//--------------------------------------------------------------
+/*
+    Modular thin client
+    Proyect Hormodular
+    Author: David Estevez Fernandez
+    Licence: GPLv3 
+    
+*/
 
-#include <Servo.h> 
+//-- Dependencies
+#include <Servo.h>
 
-//-- Mapping between the servo name (in the skymega board) and the
-//-- arduino pins
-const int SERVO_0 = 9;
-const int SERVO_1 = 10;
+//-- Definitions of configuration parameters
+#define N_SERVOS 2
+#define BAUD_RATE 57600
 
-//-- Array for accesing the 2 servos
-Servo myservo[2];  
+static const uint8_t pin_map[N_SERVOS] = { 8, 9 };
+static const uint8_t initial_pos[N_SERVOS] = { 90, 90 };
 
-int incomingByte = 0;   // for incoming serial data
+#define LED_PIN 13
 
-void setup() 
+//-- Create global objects:
+#define BUFF_SIZE 16
+
+Servo servo[N_SERVOS];
+char buffer[BUFF_SIZE];
+
+//-- Hardware setup
+void setup()
 {
-  //-- Attaching the 2 servos
-  myservo[0].attach(SERVO_0);
-  myservo[1].attach(SERVO_1);
+    //-- Setup the signal LED
+    pinMode( LED_PIN, OUTPUT);
+    digitalWrite( LED_PIN, LOW);
+    
+    //-- Initialize the servos:
+    for( uint8_t i = 0; i < N_SERVOS; i++)
+    {
+      servo[i].attach( pin_map[i] );
+      servo[i].write( initial_pos[i] );
+    }
+    
+    //-- Setup the serial port:
+    Serial.begin( BAUD_RATE );
+    Serial.flush();
+    Serial.println( "[Debug] Ok!");
+    
+    //-- Clear the buffer:
+    memset( buffer, 0, BUFF_SIZE * sizeof( char) );
+}
+
+//-- Main loop
+void loop()
+{
   
-  Serial.begin(9600);
-}  
-
-void loop() { 
+  //-- Read command
+  char command = readNext();
   
-    if (Serial.available() > 0) {  // wait to receive data...
-        // read the incoming byte:
-        incomingByte = Serial.read();
+  if ( command >= 0x50 && command <= 0x5F )
+  {
+    //-- Correct command
+    switch ( command )
+    {
+      case 0x50:
+        //-- Set position of all joints
+        posAllJointsHandler();
+        break;
+                      
+      case 0x51:
+        //-- Set position of a single joint
+        posSingleJointHandler();
+        break;
+                      
+      case 0x5F:
+        //-- Toggle LED (test command)
+        toggleLEDHandler();
+        break;
+      }
+  }  
+  
+}
 
-        // say what you got:
-        Serial.print("I received 0x");
-        Serial.println(incomingByte, HEX);
-        
-        int dir = incomingByte >> 6;
-        Serial.print("--> direction: ");
-        Serial.println(dir, DEC);
+char readNext( )
+{
+  while ( Serial.available() == 0 ) {}
+  
+  return Serial.read();
+}
 
-        int vel = (incomingByte & 0x3f) * 46;
-        Serial.print("--> velocity: ");
-        Serial.println(vel, DEC);
+void posAllJointsHandler()
+{       
+  //-- Joint pos to every joint
+  for ( uint8_t i = 0; i < N_SERVOS; i++)
+  {
+    uint8_t joint_pos = readNext();
+    servo[i].write( joint_pos);
+  }
+}
+       
+void posSingleJointHandler()
+{
+  //-- Joint pos to a single joint
+  uint8_t joint_id = readNext();
+  uint8_t joint_pos = readNext();
+  servo[joint_id].write( joint_pos);
+}
 
-        myservo[dir].writeMicroseconds(vel);
-        
-/*        if(incomingByte==97) {
-            Serial.println("It was a");
-            myservo[0].writeMicroseconds(1500);
-        } else if (incomingByte==98) {
-            Serial.println("It was b");
-            myservo[0].writeMicroseconds(2100);
-        } else if (incomingByte==99) {
-            Serial.println("It was c");
-            myservo[0].writeMicroseconds(900);
-        }*/
-        
-    }       
-} 
-
-
-
+void toggleLEDHandler()
+{
+  digitalWrite(LED_PIN, !digitalRead(LED_PIN));
+}
 
