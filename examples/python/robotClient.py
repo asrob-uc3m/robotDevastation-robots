9740c79eef26d2b832f84c980d9d9043b5e2b0ea
jgvictores <jcgvicto@gmail.com>
2017-02-01 19:03:33 +0100

    add examples/python/robotClient.py

---
+++ examples/python/robotClient.py
@@ -0,0 +1,27 @@
+#! /usr/bin/env python
+
+import yarp
+import rd
+
+yarp.Network.init()
+
+if yarp.Network.checkNetwork() != True:
+    print "[error] Please try running yarp server"
+    quit()
+
+robotOptions = yarp.Property()
+robotOptions.put('device','RobotClient')
+robotOptions.put('name','/RobotServer')
+robotDevice = yarp.PolyDriver(robotOptions)  # calls open -> connects
+
+robot = rd.viewRdRobotManager(robotDevice)  # view the actual interface
+
+print "moveForward"
+robot.moveForward()
+
+print "delay(5)"
+yarp.Time.delay(5)
+
+print "moveForward"
+robot.stopMovement()
+
